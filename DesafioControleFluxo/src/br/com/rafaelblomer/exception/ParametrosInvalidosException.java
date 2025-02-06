a4f3b2d2d17128925449eab896c251ab37f71af6
rafael-blomer <rafael.blomer@gmail.com>
2025-02-06 14:45:41 -0300

    Exercício controle de fluxo

---
+++ DesafioControleFluxo/src/br/com/rafaelblomer/exception/ParametrosInvalidosException.java
@@ -0,0 +1,9 @@
+package br.com.rafaelblomer.exception;
+
+public class ParametrosInvalidosException extends Exception{
+	private static final long serialVersionUID = 4017866577461241305L;
+
+	public ParametrosInvalidosException(String msg) {
+		super(msg);
+	}
+}
